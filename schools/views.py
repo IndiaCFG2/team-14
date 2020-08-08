62649c805d87dff1218533e9796b6eee5c827c1b
NIKHILREDDY1729 <38039672+NIKHILREDDY1729@users.noreply.github.com>
2020-08-08 22:28:20 +0530

    views

--- schools/views.py
+++ schools/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from schools.models import Schools
+# Create your views here.
+
+def schools(request):
+    school = Schools.objects.all()
+    context ={'school',school}
+    return render(request, 'schools.html')
