a24b2f71e7ea3810821238e43f396b4bf9acdc4f
SriSwathi25 <25nsriswathi@gmail.com>
2020-08-09 00:58:03 +0530

    changed views.py for courses

--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
-
+from dashboard.models import Course_Analysis as ca
 # Create your views here.
 def courseshome(request):
-	return render(request, 'dashboard/maindash.html', {})
+    #course-discriminant_analysis
+	course = ca.objects.all()
+	if request.method=="POST" :
+		date=""
+		course=0
+		week=0
+		Dname=0
+		if request.get('date')!=None and request.get('course')!=None and request.get('week')!=None and request.get('Dname')!=None :
+			date = request.get('date')
+			course_id = request.get('course')
+			week = request.get('week')
+			Dname = request.get('Dname')
+			course = ca.objects.filter(date__iexact=date, course=course_id, week=week, Dname=Dname)
+
+		elif request.get('date')!=None and request.get('course')!=None and request.get('week')!=None:
+				date = request.get('date')
+				course_id = request.get('course')
+				week = request.get('week')
+				course = ca.objects.filter(date__iexact=date, course=course_id, week=week)
+		elif request.get('date')!=None and request.get('course')!=None :
+			date = request.get('date')
+			course_id = request.get('course')
+			course = ca.objects.filter(date__iexact=date, course=course_id)
+
+		elif request.get('course')!=None :
+			course_id = request.get('course')
+			course = ca.objects.filter(date__iexact=date, course=course_id)
+    			
+	return render(request, 'dashboard/maindash.html', {'course':course})
